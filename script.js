b3d411d2d59f6b615830ec2cbf4fb62034a92fe9
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 17:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function onSubmit(event) {
+	event.preventDefault()
+	f = document.getElementById("fname").value
+	l = document.getElementById("lname").value
+	p = document.getElementById("phone").value
+	e = document.getElementById("emailid").value
+	alert(`First Name: ${f} Last Name: ${l} Phone Number: ${p} Email ID: ${e}`)
+}
